9efec5680fbc907467ef0f2101f146abc6919185
mahdi <mahdi.rezayi.853@gmail.com>
2020-10-25 20:37:08 +0330

    Updates

---
+++ precache-manifest.d1811fa42c790dde507d76b6414af2b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f1bd77da172c3e2290cf417451ab3e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "df3b1760d94546ed93c5",
+    "url": "/monsters-rolodex/static/css/main.a881bd59.chunk.css"
+  },
+  {
+    "revision": "5736ec3e1d86e7ef1fd8",
+    "url": "/monsters-rolodex/static/js/2.2642a0e5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2642a0e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df3b1760d94546ed93c5",
+    "url": "/monsters-rolodex/static/js/main.4997fc56.chunk.js"
+  },
+  {
+    "revision": "5d9871b43329dda10fc8",
+    "url": "/monsters-rolodex/static/js/runtime-main.98ead556.js"
+  }
+]);
